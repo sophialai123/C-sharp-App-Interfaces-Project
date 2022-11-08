af6e8084e4b1ce092435f0d11f71b71435769594
Sophia <83651475+sophialai123@users.noreply.github.com>
2022-11-08 20:22:02 +1100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+
+namespace SavingInterface
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      TodoList tdl = new TodoList();
+      tdl.Add("Invite friends");
+      tdl.Add("Buy decorations");
+      tdl.Add("Party");
+
+      PasswordManager pm = new PasswordManager("iluvpie", true);
+    }
+  }
+}
