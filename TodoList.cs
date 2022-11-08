39af3c6b99c6fd693f8a803ff461bf384d36f2f0
Sophia <83651475+sophialai123@users.noreply.github.com>
2022-11-08 23:56:10 +1100

    Update TodoList.cs

--- TodoList.cs
+++ TodoList.cs
@@ -2,12 +2,19 @@ using System;
 
 namespace SavingInterface
 {
-  class TodoList
+  class TodoList: IDisplayable,IRestable
   {
     public string[] Todos
     { get; private set; }
 
+   public string headerSymbol = "-------";
+    public string HeaderSymbol
+    {get{
+      return headerSymbol;
+    }}
+
     private int nextOpenIndex;
+    
 
     public TodoList()
     {
@@ -19,6 +26,30 @@ namespace SavingInterface
     {
       Todos[nextOpenIndex] = todo;
       nextOpenIndex++;
+    }
+
+    //implement interface method
+    public void Display()
+    {
+     
+       Console.WriteLine("Todos");
+       Console.WriteLine(HeaderSymbol);
+      foreach(string todo in Todos)
+      {
+        if(String.IsNullOrEmpty(todo)){
+          Console.WriteLine("[]");
+        }else{
+           Console.WriteLine(todo);
+        }
+       
+      }
+     
+    }
+
+    public void Reset()
+    {
+      Todos = new string[5];
+      nextOpenIndex = 0;
     }
   }
 }
