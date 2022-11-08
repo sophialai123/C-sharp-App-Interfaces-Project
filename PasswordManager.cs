0390387d01d6c38995d59a89737e275cca640a5b
Sophia <83651475+sophialai123@users.noreply.github.com>
2022-11-08 23:56:32 +1100

    Update PasswordManager.cs

--- PasswordManager.cs
+++ PasswordManager.cs
@@ -2,18 +2,73 @@ using System;
 
 namespace SavingInterface
 {
-  class PasswordManager
+  class PasswordManager: IDisplayable,IRestable
   {
     private string Password
-    { get; set; }
+    { 
+      get
+      {
+        return Password;
+      }
+    set
+    {
+      //password must be at least 8
+      if(value.Length >= 8){Password= value;}
+      
+    } 
+    }
 
     public bool Hidden
     { get; private set; }
+
+    public string headerSymbol = "-------";
+    public string HeaderSymbol
+    {get{
+      return headerSymbol;
+    }}
+
 
     public PasswordManager(string password, bool hidden)
     {
       Password = password;
       Hidden = hidden;
     }
+
+    //method
+   public bool ChangePassword(string oldPassword, string newPassword)
+   {
+     if(oldPassword == newPassword)
+     {
+       Password = newPassword;
+       return true;
+     }
+     else 
+     {
+       return false;
+     }
+   }
+    
+    //implement interface method
+    public void Display()
+    {
+        Console.WriteLine("Password");
+       Console.WriteLine(HeaderSymbol);
+      if(!Hidden)
+      {
+        Console.WriteLine(Password);
+      }     
+      else
+      {
+         Console.WriteLine("*********");
+      }
+
+    }
+
+    public void Reset()
+    {
+      Password = "";
+      Hidden = false;
+    }
+
   }
 }
